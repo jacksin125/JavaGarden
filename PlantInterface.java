a374c674bba6dea9d30c6fc2d026fcc69a211d9e
MASTERUFALL <masterufall@gmail.com>
2017-03-16 09:20:21 -0400

    Add files via upload

---
+++ PlantInterface.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of interface PlantInterface here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface PlantInterface
+{
+    void grow(int days);
+    
+    void rain(int days);
+    
+    void frost();
+    
+    void draw();
+}
